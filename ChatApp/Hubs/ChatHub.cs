09a0bde2e8c6e289f0c4c101167d12a0373125db
Logan Crandall <crandall.logan@gmail.com>
2022-04-28 20:15:57 -0400

    Register new connection with signalr method instead of OnConnectedAsync

--- ChatApp/Hubs/ChatHub.cs
+++ ChatApp/Hubs/ChatHub.cs
@@ -1,50 +1,47 @@
-using System.Net;
-using System.Collections;
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR; 
 using Microsoft.Extensions.Logging; 
 using ChatApp.Models; 
 using ChatApp.Repositories;
-using Microsoft.AspNetCore.Http.Features;
-using ChatApp.Helpers;
 
 namespace ChatApp.Hubs{
-    class ChatHub : Hub{
+    public class ChatHub : Hub{
         public const string hubPath = "/ChatHub"; 
         private readonly ILogger<ChatHub> _logger;
-        private readonly ConnectedUserRepository _connectedUserRepository; 
+        private readonly ConnectedUserRepository _connectedUserRepository;
 
         //construct
         public ChatHub(ILogger<ChatHub> logger, ConnectedUserRepository connectedUserRepository){
             _logger = logger; 
             _connectedUserRepository = connectedUserRepository; 
         }
-        public async override Task OnConnectedAsync()
+        
+        public override async Task OnConnectedAsync()
         {
-            _logger.LogInformation("User Connected... "); 
-            _connectedUserRepository.Create(new ConnectedUser{
-                IPAddress = IPAddressHelper.ToString(Context.Features.Get<IHttpConnectionFeature>().RemoteIpAddress)
-            }); 
+            _logger.LogInformation("User Connected with ConnectionId = {ConnectionId}", Context.ConnectionId);
+
             await Clients.All.SendAsync("sendMessage", "TestMessage", "TestUser");
             await base.OnConnectedAsync(); 
         }
 
         public override Task OnDisconnectedAsync(Exception exception)
         {
-            _logger.LogInformation("User Disonnected... "); 
-            _connectedUserRepository.Delete(new ConnectedUser{
-                IPAddress = IPAddressHelper.ToString(Context.Features.Get<IHttpConnectionFeature>().RemoteIpAddress)
-            }); 
+            _logger.LogInformation("User Disconnected with ConnectionId = {ConnectionId}", Context.ConnectionId);
+
+            _connectedUserRepository.Delete(Context.ConnectionId);
             return base.OnDisconnectedAsync(exception);
         }
 
         public async Task SendMessage(Message message){
             _logger.LogInformation($"{message}");
             await Clients.All.SendAsync("SendMessage", message);
-        } 
+        }
+
+        public async Task OnNewConnectedUser(ConnectedUser connectedUser)
+        {
+            _logger.LogInformation("User connected with ConnectionId = {ConnectionId}, and IpAddress = {IpAddress}", Context.ConnectionId, connectedUser.IPAddress);
+            _connectedUserRepository.Create(Context.ConnectionId, connectedUser);
+        }
     }
-
-
-
 }
